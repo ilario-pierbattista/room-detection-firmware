b5fe8a0d9205d124e56d71a769aca18a8b48962e
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2017-03-18 00:00:48 +0100

    broadcasting rssi

--- src/config.h
+++ src/config.h
@@ -6,9 +6,15 @@
 #define APIO_DONGLES_PINOCCIO_CONFIG_H
 
 // Indirizzo della scheda
-#define DONGLE_ADDRESS 0x01
+#define DONGLE_ADDRESS 0x03
 
 // Indirizzo del coordinatore
 #define COORDINATOR_ADDRESS 0x01
+
+#define PING_ENDPOINT 1
+#define REPORT_ENDPOINT 2
+
+#define DEBUG_ANCHOR
+// #define DEBUG_COORD
 
 #endif //APIO_DONGLES_PINOCCIO_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include "lwm.h"
-#include "lwm/sys/sys.h"
-#include "lwm/nwk/nwk.h"
+#include <lwm/sys/sysTimer.h>
+#include <lwm/nwk/nwkTx.h>
+#include <lwm/sys/sys.h>
+#include <QueueArray.h>
+
 #include "config.h"
 
 /**
@@ -17,14 +18,7 @@
 // r{<Address>:<Valore RSSI>[, <Address>:<Valore RSSI>]}
 // (Invio con ack)
 
-
-#define ANCHOR_TO_COORDINATOR_ENDPOINT 1
-#define ANCHOR_TO_ANCHOR_ENDPOINT 2
-
 #define PING_MSG_SIZE 3
-
-#define DEBUG_ANCHOR
-// #define DEBUG_COORD
 
 extern "C" {
 void println (char *x) {
@@ -33,35 +27,40 @@ void println (char *x) {
 }
 }
 
+typedef enum AppState_t {
+    APP_STATE_INITIAL, APP_STATE_IDLE,
+} AppState_t;
+
+static void APP_TaskHandler (void);
+
+static void APP_Init (void);
+
+static void ping_confirmation (NWK_DataReq_t *req);
+
+static void anchor_tx_ping ();
+
 /**
  * Stampa le informazioni sul dongle all'avvio
  */
 static void print_info_boot (void);
-
-static void ping_coordinator ();
-
-static void ping_coordinator_ack (NWK_DataReq_t *ind);
 
 /**
  * Handler del coordinator per la ricezione del ping da un anchor node
  * @param ind Pacchetto di dati ricevuti
  * @return Stato
  */
-static bool coordinator_receive_ping (NWK_DataInd_t *ind);
+static bool coordinator_rx_ping (NWK_DataInd_t *ind);
 
-/**
- * Converte un uint16 in un array di due bytes
- * @param dest Indirizzo dell'array di byte di destinazione
- * @param src Valore da convertire (unsigned short)
- */
-static void uint16_to_bytes (uint8_t *dest, uint16_t src);
+static bool anchor_rx_ping (NWK_DataInd_t *ind);
 
-/**
- * Converte un array di due byte in un uint16
- * @param src Indirizzo dell'array di byte da convertire
- * @return Valore convertito in unsigned short
+static void ping_timer_handler (SYS_Timer_t *timer);
+
+/***********************************************************************
+ *
+ *      UTILS HEADERS
+ *
+ ***********************************************************************
  */
-static uint16_t bytes_to_uint16 (uint8_t *src);
 
 /**
  * Scrive in seriale (per motivi di debug) le informazioni contenute in
@@ -80,65 +79,140 @@ static void debug_print_dataind_summary (NWK_DataInd_t *ind);
 static void
 debug_bytes_to_hex_digest (char *dest, uint8_t *msg, size_t size);
 
-uint16_t coordinator_ping_counter = 0;
-uint8_t coordinator_ping_msg[PING_MSG_SIZE];
+/**
+ * Converte un uint16 in un array di due bytes
+ * @param dest Indirizzo dell'array di byte di destinazione
+ * @param src Valore da convertire (unsigned short)
+ */
+static void uint16_to_bytes (uint8_t *dest, uint16_t src);
+
+/**
+ * Converte un array di due byte in un uint16
+ * @param src Indirizzo dell'array di byte da convertire
+ * @return Valore convertito in unsigned short
+ */
+static uint16_t bytes_to_uint16 (uint8_t *src);
+
+/***********************************************************************
+ *
+ *      STATIC GLOBAL VARIABLES
+ *
+ ***********************************************************************
+ */
+
+static AppState_t appState;
+static uint16_t coordinator_ping_counter = 0;
+static uint8_t coordinator_ping_msg[PING_MSG_SIZE];
+static QueueArray<NWK_DataReq_t *> outcoming_stack;
+static SYS_Timer_t ping_timer;
+static bool ping_req_busy = false;
+
+static void ping_timer_handler (SYS_Timer_t *timer) {
+    anchor_tx_ping();
+    (void) timer;
+}
+
+/***********************************************************************
+ *
+ *      MAIN LOOP
+ *
+ ***********************************************************************
+ */
 
 void setup () {
     Serial.begin(115200);
-    print_info_boot();
+
     SYS_Init();
-    NWK_SetAddr(DONGLE_ADDRESS);
-    NWK_SetPanId(0x01);
-    PHY_SetChannel(0x1a);
-    PHY_SetRxState(true);
-    NWK_OpenEndpoint(ANCHOR_TO_COORDINATOR_ENDPOINT,
-                     coordinator_receive_ping);
-    // @todo Aggiungere endpoint per il ping delle ancore
-    // @todo Aggiungere endpoint per invio del report al coordinator
+
+    print_info_boot();
 }
 
 void loop () {
     SYS_TaskHandler();
-    if (DONGLE_ADDRESS != COORDINATOR_ADDRESS) {
-        // Sono un dongle
-        ping_coordinator();
-        delay(300);
-    } else {
-        // Sono un coordinator
-        delay(10);
+    APP_TaskHandler();
+}
+
+static void APP_TaskHandler (void) {
+    switch (appState) {
+        case APP_STATE_INITIAL: {
+            APP_Init();
+            appState = APP_STATE_IDLE;
+        }
+            break;
+
+        case APP_STATE_IDLE:
+            break;
+
+        default:
+            break;
     }
 }
 
-static void ping_coordinator () {
-    coordinator_ping_counter++;
+static void APP_Init (void) {
+    NWK_SetAddr(DONGLE_ADDRESS);
+    NWK_SetPanId(0x01);
+    PHY_SetChannel(0x1a);
+    PHY_SetRxState(true);
+
+    if (DONGLE_ADDRESS == COORDINATOR_ADDRESS) {
+        NWK_OpenEndpoint(PING_ENDPOINT, coordinator_rx_ping);
+    } else {
+        NWK_OpenEndpoint(PING_ENDPOINT, anchor_rx_ping);
+    }
+
+    if (!SYS_TimerStarted(&ping_timer)) {
+        ping_timer.interval = 300;
+        ping_timer.mode = SYS_TIMER_PERIODIC_MODE;
+        ping_timer.handler = ping_timer_handler;
+        SYS_TimerStart(&ping_timer);
+    }
+}
+
+static void anchor_tx_ping () {
+    if (ping_req_busy) return;
+
+    // Incodamento della richieste
+    if (outcoming_stack.count() >= NWK_BUFFERS_AMOUNT) {
+        free(outcoming_stack.dequeue());
+    }
+    NWK_DataReq_t *outcoming_msg = (NWK_DataReq_t *) malloc(
+            sizeof(NWK_DataReq_t));
+    outcoming_stack.enqueue(outcoming_msg);
 
     // Creazione del messaggio
+    coordinator_ping_counter++;
     coordinator_ping_msg[0] = 'C';
     uint16_to_bytes(&coordinator_ping_msg[1], coordinator_ping_counter);
 
-    // @todo è un leak di memoria. Da risolvere
-    NWK_DataReq_t *message = (NWK_DataReq_t *) malloc(
-            sizeof(NWK_DataReq_t));
-    message->dstAddr = COORDINATOR_ADDRESS;
-    message->dstEndpoint = ANCHOR_TO_COORDINATOR_ENDPOINT;
-    message->srcEndpoint = 1;
-    message->options = 0;
-    message->data = coordinator_ping_msg;
-    message->size = PING_MSG_SIZE;
-    message->confirm = ping_coordinator_ack;
+    // Impacchettamento
+    outcoming_msg->dstAddr = NWK_BROADCAST_ADDR;
+    outcoming_msg->dstEndpoint = PING_ENDPOINT;
+    outcoming_msg->srcEndpoint = 1;
+    outcoming_msg->options = 0;
+    outcoming_msg->confirm = ping_confirmation;
+    outcoming_msg->data = coordinator_ping_msg;
+    outcoming_msg->size = PING_MSG_SIZE;
 
     #ifdef DEBUG_ANCHOR
     char msg_debug[30];
-    debug_bytes_to_hex_digest(msg_debug, message->data, message->size);
+    debug_bytes_to_hex_digest(msg_debug, outcoming_msg->data,
+                              outcoming_msg->size);
     Serial.println("Sent msg: " + String(msg_debug));
     #endif
 
-    NWK_DataReq(message);
+    NWK_DataReq(outcoming_msg);
+
+    ping_req_busy = true;
 }
 
-static bool coordinator_receive_ping (NWK_DataInd_t *ind) {
+static void ping_confirmation (NWK_DataReq_t *req) {
+    ping_req_busy = false;
+    (void) req;
+}
+
+static bool coordinator_rx_ping (NWK_DataInd_t *ind) {
     #ifdef DEBUG_COORD
-    debug_received_frame(ind);
+    debug_print_dataind_summary(ind);
     #endif
     char out[50];
     // Stampando il letterale di un dizionario riesco a minimizzare
@@ -149,6 +223,19 @@ static bool coordinator_receive_ping (NWK_DataInd_t *ind) {
     return true;
 }
 
+static bool anchor_rx_ping (NWK_DataInd_t *ind) {
+    #ifdef DEBUG_ANCHOR
+    debug_print_dataind_summary(ind);
+    #endif
+    return true;
+}
+
+/***********************************************************************
+ *
+ *      UTILS DEFINITION
+ *
+ ***********************************************************************
+ */
 static void print_info_boot (void) {
     Serial.println(
             F("\n\n========================================\n#"));
@@ -163,12 +250,6 @@ static void print_info_boot (void) {
     Serial.println(F("========================================"));
 }
 
-// @TODO unit testing http://docs.platformio.org/en/latest/plus/unit-testing.html#unit-testing
-static void uint16_to_bytes (uint8_t *dest, uint16_t src) {
-    *dest = (src >> 8) & 0xff;
-    *(dest + 1) = (src) & 0xff;
-}
-
 void debug_print_dataind_summary (NWK_DataInd_t *ind) {
     char debug_msg[100], msg_body[30];
     debug_bytes_to_hex_digest(msg_body, ind->data, ind->size);
@@ -179,14 +260,6 @@ void debug_print_dataind_summary (NWK_DataInd_t *ind) {
     Serial.println(debug_msg);
 }
 
-void ping_coordinator_ack (NWK_DataReq_t *ind) {
-    #ifdef DEBUG_ANCHOR
-    Serial.println(
-            F("Il coordinator ha ricevuto il ping e ha inviato un ACK"));
-    #endif
-    free(ind);
-}
-
 void debug_bytes_to_hex_digest (char *dest, uint8_t *msg, size_t size) {
     dest[0] = '\0';
     for (size_t i = 0; i < size; i++) {
@@ -194,8 +267,14 @@ void debug_bytes_to_hex_digest (char *dest, uint8_t *msg, size_t size) {
     }
 }
 
+static void uint16_to_bytes (uint8_t *dest, uint16_t src) {
+    *dest = (src >> 8) & 0xff;
+    *(dest + 1) = (src) & 0xff;
+}
+
 uint16_t bytes_to_uint16 (uint8_t *src) {
     uint16_t dest = (src[0] << 8);
     dest |= src[1];
     return dest;
 }
+/**********************************************************************/
